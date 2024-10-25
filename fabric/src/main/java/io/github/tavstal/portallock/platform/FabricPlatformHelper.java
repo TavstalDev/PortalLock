8b0927114b9d3188b7d6613eed46f0b6f5b41a8d
Tavstal <tavstal@gmail.com>
2024-10-25 15:11:50 +0200

    Added hasPermission method

--- fabric/src/main/java/io/github/tavstal/portallock/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/io/github/tavstal/portallock/platform/FabricPlatformHelper.java
@@ -2,6 +2,7 @@ package io.github.tavstal.portallock.platform;
 
 import io.github.tavstal.portallock.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.level.ServerPlayer;
 
 public class FabricPlatformHelper implements IPlatformHelper {
 
@@ -20,5 +21,10 @@ public class FabricPlatformHelper implements IPlatformHelper {
     public boolean isDevelopmentEnvironment() {
 
         return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+
+    @Override
+    public boolean hasPermission(ServerPlayer player, String permission) {
+        return  player.hasPermissions(4);
     }
 }
--- plugin/src/main/java/io.github.tavstal.portallock/platform/PluginPlatformHelper.java
+++ plugin/src/main/java/io.github.tavstal.portallock/platform/PluginPlatformHelper.java
@@ -1,25 +1,14 @@
 package io.github.tavstal.portallock.platform;
 
 import io.github.tavstal.portallock.platform.services.IPlatformHelper;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 
 public class PluginPlatformHelper implements IPlatformHelper {
 
     @Override
     public String getPlatformName() {
         return "Paper";
-    }
-
-    // Note: plugin is always server side
-    @Override
-    public boolean isClientSide() {
-
-        return false;
-    }
-
-    @Override
-    public boolean isServerSide() {
-
-        return true;
     }
 
     @Override
@@ -37,5 +26,11 @@ public class PluginPlatformHelper implements IPlatformHelper {
     public boolean isDevelopmentEnvironment() {
 
         return false;
+    }
+
+    @Override
+    public boolean hasPermission(ServerPlayer player, String permission) {
+        CraftPlayer craftPlayer = player.getBukkitEntity();
+        return  craftPlayer.hasPermission(permission);
     }
 }
